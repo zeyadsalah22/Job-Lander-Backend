e05641629ffc0c5aac459907a92a1bf1a22b1d65
zeyadsalah22 <112489529+zeyadsalah22@users.noreply.github.com>
2025-02-02 15:11:57 +0200

    adjustorigins

--- joblander/settings.py
+++ joblander/settings.py
@@ -32,7 +32,8 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.fly.dev']  # <-- Updated!
+CSRF_TRUSTED_ORIGINS = ['https://*.fly.dev']  # <-- Updated!
 
 
 # Application definition
